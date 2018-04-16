94b3e1cc0593f00bdde49d6f9220ee8f6288c1eb
Oleksandr Shevchenko <shevchenko.olelsandr96@gmail.com>
2018-04-16 22:52:18 +0300

    Initial commit

---
+++ src/main/java/com/oshevchenko/couchdemo/CouchdemoApplication.java
@@ -0,0 +1,12 @@
+package com.oshevchenko.couchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouchdemoApplication.class, args);
+    }
+}
